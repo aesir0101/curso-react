aa88726996bee9876f905e01b8ec3d7db9c5b846
pedro <84733192+Dantes008@users.noreply.github.com>
2022-08-19 10:03:23 -0300

    Add files via upload

---
+++ convidados/src/contatos/index.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react'
+import { Contato } from './contato'
+import { ContatosForm } from './Form'
+import { ContatosList } from './List'
+import { useContatosLogic } from './useContatosLogic'
+
+export const Contatos: FC = () => {
+
+    const { 
+        addContato, 
+        contato, 
+        contatosList, 
+        removeContato 
+    } = useContatosLogic();
+
+    const handleFormSubmit = (contato: Contato) => {
+        addContato(contato)
+    }
+
+    return (
+        <>
+            <ContatosForm submeterFormulario={handleFormSubmit} 
+                          contato={contato} />
+            <ContatosList list={contatosList} onDelete={removeContato} />
+        </>
+    )
+}
